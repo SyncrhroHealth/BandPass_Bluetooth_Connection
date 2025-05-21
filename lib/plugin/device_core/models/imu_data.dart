5fcd0c7c570cf301e386f24d36ec856076010276
Tuyen Vo <khactuyen.vo@citynow.vn>
2025-05-22 00:11:48 +0700

    fix bugs

--- lib/plugin/device_core/models/imu_data.dart
+++ lib/plugin/device_core/models/imu_data.dart
@@ -1,33 +1,49 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:bandpass_ble_connection/plugin/device_core/models/vec_3_batch.dart';
 
 class ImuData {
   final int count;
-  final Vec3Batch accel;
-  final Vec3Batch gyro;
+  final double accelX;
+  final double accelY;
+  final double accelZ;
+  final double gyroX;
+  final double gyroY;
+  final double gyroZ;
   final int adcRaw;
   final int timestampMs;
 
   ImuData({
     required this.count,
-    required this.accel,
-    required this.gyro,
+    required this.accelX,
+    required this.accelY,
+    required this.accelZ,
+    required this.gyroX,
+    required this.gyroY,
+    required this.gyroZ,
     required this.adcRaw,
     required this.timestampMs,
   });
 
-  static ImuData fromMap(dynamic map) {
+  factory ImuData.fromMap(Map<String, dynamic> map) {
     return ImuData(
-      count: map['count'],
-      accel: map['accel'],
-      gyro: map['gyro'],
-      adcRaw: map['adcRaw'],
-      timestampMs: map['timestampMs'],
+      count: map['count'] as int,
+      accelX: (map['accelX'] as num).toDouble(),
+      accelY: (map['accelY'] as num).toDouble(),
+      accelZ: (map['accelZ'] as num).toDouble(),
+      gyroX: (map['gyroX'] as num).toDouble(),
+      gyroY: (map['gyroY'] as num).toDouble(),
+      gyroZ: (map['gyroZ'] as num).toDouble(),
+      adcRaw: map['adcRaw'] as int,
+      timestampMs: map['timestampMs'] as int,
     );
   }
 
   @override
   String toString() {
-    return 'ImuData(count: $count, accel: $accel, gyro: $gyro, adcRaw: $adcRaw, timestampMs: $timestampMs)';
+    return 'ImuData('
+        'count: $count, '
+        'accelX: $accelX, accelY: $accelY, accelZ: $accelZ, '
+        'gyroX: $gyroX, gyroY: $gyroY, gyroZ: $gyroZ, '
+        'adcRaw: $adcRaw, timestampMs: $timestampMs'
+        ')';
   }
 }
